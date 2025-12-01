826841e554f75282335a0d1f3efe0b24e12a0b4c
unknown <ckdgusrns@naver.com>
2025-12-01 10:55:13 +0900

    수정: OPTIONS 요청(CORS preflight) 명시적 처리 추가

--- server/index.js
+++ server/index.js
@@ -45,6 +45,19 @@ app.use(cors({
   preflightContinue: false,
   optionsSuccessStatus: 204,
 }));
+
+// OPTIONS 요청을 명시적으로 처리 (CORS preflight)
+app.options('*', (req, res) => {
+  const origin = req.headers.origin;
+  if (origin && allowedOrigins.includes(origin)) {
+    res.setHeader('Access-Control-Allow-Origin', origin);
+    res.setHeader('Access-Control-Allow-Credentials', 'true');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS, PATCH');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization, X-Requested-With, Accept, Origin');
+  }
+  res.status(204).end();
+});
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
